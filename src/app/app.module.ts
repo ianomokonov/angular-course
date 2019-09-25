c27391bd720f4298173d2e385985df496b04d2d6
Nomokonov <INomokonov@luxoft.com>
2019-09-26 00:10:06 +0300

    Каркас

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlacesListComponent } from './places-list/places-list.component';
+import { WeatherComponent } from './weather/weather.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlacesListComponent,
+    WeatherComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
